f13b64b134e9f91579dadb4b2d7591c9936d9724
DionysiaM <dionmust@gmail.com>
2021-11-14 11:23:02 +0100

    Added personal url(s)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1> Weather App </h1>
+      <footer>
+        This app was coded by{" "}
+        <a href="https://festive-edison-7f3e14.netlify.app/" target="_blank">
+          Dionysia Mastorou
+        </a>{" "}
+        and it is{" "}
+        <a
+          href="https://github.com/DionysiaM/react-weather-app"
+          target="_blank"
+        >
+          open-sourced
+        </a>{" "}
+        on GitHub.{" "}
+      </footer>
+    </div>
+  );
 }
