08c3001a501f2151906b09d6a9b28e6de30faca3
DionysiaM <dionmust@gmail.com>
2021-11-14 15:12:07 +0100

    css fixed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
+import Weather from "./Weather";
+import "./index.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1> Weather App </h1>
+        <Weather />
         <footer>
-          This app was coded by{" "}
+          This application was coded by{" "}
           <a href="https://festive-edison-7f3e14.netlify.app/" target="_blank">
             Dionysia Mastorou
           </a>{" "}
