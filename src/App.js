634503231c508f060df9578ae8a71098dfcd93b2
DionysiaM <dionmust@gmail.com>
2021-11-14 11:42:16 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,21 +3,23 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App </h1>
-      <footer>
-        This app was coded by{" "}
-        <a href="https://festive-edison-7f3e14.netlify.app/" target="_blank">
-          Dionysia Mastorou
-        </a>{" "}
-        and it is{" "}
-        <a
-          href="https://github.com/DionysiaM/react-weather-app"
-          target="_blank"
-        >
-          open-sourced
-        </a>{" "}
-        on GitHub.{" "}
-      </footer>
+      <div className="container">
+        <h1> Weather App </h1>
+        <footer>
+          This app was coded by{" "}
+          <a href="https://festive-edison-7f3e14.netlify.app/" target="_blank">
+            Dionysia Mastorou
+          </a>{" "}
+          and it is{" "}
+          <a
+            href="https://github.com/DionysiaM/react-weather-app"
+            target="_blank"
+          >
+            open-sourced
+          </a>{" "}
+          on GitHub.{" "}
+        </footer>
+      </div>
     </div>
   );
 }
