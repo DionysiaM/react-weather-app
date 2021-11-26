25918705f7670bb5960bcc6e2d6ecc8e2eeaf1c3
DionysiaM <dionmust@gmail.com>
2021-11-26 19:35:41 +0100

    Forecast API

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,18 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherDailyForecast() {
+export default function WeatherDailyForecast(props) {
+  function handleResponse(response) {}
+
+  let apiKey = "293f64100f990f0f471f76f8c331c1c9";
+  let lat = props.coord.lat;
+  let lon = props.coord.lon;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude={part}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="WeatherForecast">
       <div className="row">
